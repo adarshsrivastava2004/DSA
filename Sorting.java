6ff66f1de9cb2bc69c26e7944c9a99a957600e62
Adarsh Srivastava <adarshsrivastava202@gmail.com>
2026-06-13 19:30:08 +0530

    Selection Sort

--- Sorting.java
+++ Sorting.java
@@ -6,7 +6,7 @@ public class Sorting{
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     // agr phle wala element bda h to usko swap krdo
-                    //tc: O(n^2),sc: O(1),in-place yes, stable yes
+                    //tc: O(n^2),sc: O(1),in-place yes, stable yes (same value k order ko maintain krta h)
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
@@ -14,9 +14,28 @@ public class Sorting{
             }
         }
     }
+
+
     public static void selectionSort(int[] arr){
-        
+        int n = arr.length;
+        //ek part sorted ar ek unsorted
+        for(int i=0;i<n-1;i++){
+            //ek minIdx jo ki i se hi suru hota h..
+            int mI = i;
+            //j ko minIdx k aage se start krke minIdx pr choti wali value ko rkhna
+            for(int j=i+1;j<n;j++){
+                if(arr[mI]>arr[j]){
+                    mI=j;
+                }
+            }
+            //last me arr[i] se swap
+            int temp = arr[i];
+            arr[i] = arr[mI];
+            arr[mI] = temp;
+            //tc: O(n^2),sc: O(1),in-place yes (extra memory use) , stable no (same value k order ko maintain nhi krta h)
+        }
     }
+
     public static void insertionSort(int[] arr){
         
     }
@@ -26,19 +45,19 @@ public class Sorting{
     public static void main(String[] args){
         int[] arr = {5, 2, 9, 1, 5, 6};
 
-        bubbleSort(arr);
-        System.out.print("Sorted array by Bubble Sort: ");
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
-
-        selectionSort(arr);
-        // System.out.print("\nSorted array by Selection Sort: ");
+        // bubbleSort(arr);
+        // System.out.print("Sorted array by Bubble Sort: ");
         // for(int i : arr){
         //     System.out.print(i + " ");
         // }
 
-        insertionSort(arr);
+        selectionSort(arr);
+        System.out.print("\nSorted array by Selection Sort: ");
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+
+        // insertionSort(arr);
         // System.out.print("\nSorted array by Insertion Sort: ");
         // for(int i : arr){
         //     System.out.print(i + " ");
