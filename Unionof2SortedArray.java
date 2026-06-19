66426f6dba5eb550d99600c7e535b194e5dc5ac7
Adarsh Srivastava <adarshsrivastava202@gmail.com>
2026-06-19 16:14:59 +0530

    optimized Unionof2SortedArray

--- Unionof2SortedArray.java
+++ Unionof2SortedArray.java
@@ -7,22 +7,71 @@ public class Unionof2SortedArray{
         int n = a.length;
         int m = b.length;
         //set unordered hoga agr hashset ka use kra toh isiliye treeset use kr lena chaiye as treeset ordered with unique hota h..
-        Set<Integer> set = new TreeSet<>();
+        // Set<Integer> set = new TreeSet<>();
 
-        for(int i=0;i<n;i++){ // tree set single time complexity O(logn) so here for n we have O(nlogn) 
-            set.add(a[i]);
+        // for(int i=0;i<n;i++){ // tree set single time complexity O(logn) so here for n we have O(nlogn) 
+        //     set.add(a[i]);
+        // }
+
+        // System.out.println(set + "-----------\n");
+
+        // for(int i=0;i<m;i++){
+        //     set.add(b[i]);  // we have O(mlogn) 
+        // }
+
+        // System.out.println(set + "-----------\n");
+
+        // ArrayList<Integer> union = new ArrayList<>(set);// tc for converting set to list = O(n+m)
+        //  //space = for set O(n+m) 
+
+
+        // return union;
+
+        ArrayList<Integer> union = new ArrayList<>();
+        
+        int i=0,j=0;
+        
+        while(i<n && j<m){
+            //jb a[i] chota hoga toh a[i] add kro ar iterator + kro
+            if(a[i]<b[j]){
+                if((union.isEmpty()) || (union.get(union.size()-1)!=a[i])){
+                    union.add(a[i]);
+                }
+                i++;
+            }
+            //jb b[i] chota hoga toh b[i] add kro ar iterator + kro
+            else if(a[i]>b[j]) {
+                if((union.isEmpty()) || (union.get(union.size()-1)!=b[j])){
+                    union.add(b[j]);
+                }
+                j++;
+            //jb dono brabr hoga toh koi bhi add kro ar dono iterator + kro
+            } else{
+                if((union.isEmpty()) || (union.get(union.size()-1)!=a[i])){
+                    union.add(a[i]);
+                }
+            i++;
+            j++;
+            }
+            
         }
-
-        System.out.println(set + "-----------\n");
-
-        for(int i=0;i<m;i++){
-            set.add(b[i]);  // we have O(mlogn) 
+        
+        // ab agr kisi array ka element khtm ho gya tb ek particular array ka element lgatrr check k sath add krna hoga..
+            //phle a[i]
+        while(i<n){
+            if((union.isEmpty()) || (union.get(union.size()-1)!=a[i])){
+                union.add(a[i]);
+            }
+        i++;
         }
-
-        System.out.println(set + "-----------\n");
-
-        ArrayList<Integer> union = new ArrayList<>(set);// tc for converting set to list = O(n+m)
-         //space = for set O(n+m) 
+            
+        //phir b[i]
+        while(j<m){
+            if((union.isEmpty()) || (union.get(union.size()-1)!=b[j])){
+                union.add(b[j]);
+            }
+            j++;
+        }
 
 
         return union;
