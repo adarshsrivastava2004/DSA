072b568cbeabff97fe13fbbefe05f665a6b8410b
Adarsh Srivastava <adarshsrivastava202@gmail.com>
2026-06-21 23:58:45 +0530

    MajorityElementI

---
+++ MajorityElementI.java
@@ -0,0 +1,21 @@
+public class MajorityElementI{
+    public static int majorityElement(int[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int count =1;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>n/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 1, 2};
+        System.out.println(majorityElement(arr));
+    }
+}
